c8f04bd83f45b491216c2c89a518dd96d304953b
unknown <b.c.todoran@gmail.com>
2022-09-21 14:09:48 +0300

    Figured out connection

--- Assets/Scripts/UI/BuildList_Popup.cs
+++ Assets/Scripts/UI/BuildList_Popup.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildList_Popup : MonoBehaviour
 {
@@ -10,20 +10,31 @@ public class BuildList_Popup : MonoBehaviour
     [SerializeField] private float componentHeight = 550.0f;
     [SerializeField] private float childHeight = 150.0f;
 
+    [SerializeField] private Button buildingButtonPrefab;
+
     #endregion Fields
 
     #region Mono
 
     private void Awake()
     {
-        childCount = transform.childCount;
-        Debug.Log("BuildList button count: " + childCount);
-        AdaptComponentHeight();
+        
     }
 
     #endregion Mono
 
     #region Methods
+
+    public void Initialize(List<HexData> AcceptedHex)
+    {
+        foreach (var item in AcceptedHex)
+        {
+            Instantiate(buildingButtonPrefab);
+            buildingButtonPrefab.GetComponentInChildren<Image>().sprite = item.Sprite;
+            buildingButtonPrefab.transform.parent = this.transform;
+        }
+        AdaptComponentHeight();
+    }
 
     private void AdaptComponentHeight()
     {
